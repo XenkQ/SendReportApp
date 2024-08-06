ae07f4df5d2b083fa1cace32f85a4952e8688feb
admin2 <admin2@KMP-005KB.uml.lan>
2024-08-06 12:32:07 +0200

    Creating first categories page

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSendButtonClick(object sender, EventArgs e)
+    {
+        Console.WriteLine("Sending");
+    }
+
+    private void OnNextButtonClick(object sender, EventArgs e)
+    {
+        Console.WriteLine("Next");
+    }
+
+    private async void OnTakePhotoClick(object sender, EventArgs e) 
+    {
+        await TakePhoto();
+    }
+
+    private async Task TakePhoto()
+    {
+        try
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+
+            if(photo != null)
+            {
+                var stream = await photo.OpenReadAsync();
+                //photoResultImage.Source = ImageSource.FromStream(() => stream);
+                Console.WriteLine(photo.FileName);
+            }
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Error", $"Can't take photo: {ex.Message}", "OK");
+        }
+    }
+}
