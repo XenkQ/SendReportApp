b57ef164007c014d8cf4d8717f8b7c031d932592
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2024-08-24 22:35:16 +0200

    Deleting alert data to send interface

--- Data/Storing/AlertDataToSend.cs
+++ Data/Storing/AlertDataToSend.cs
@@ -1,12 +1,11 @@
-﻿namespace MauiApp1.Data.Storing
+﻿namespace MauiApp1.Data.Storing;
+
+public sealed class AlertDataToSend
 {
-    internal class AlertDataToSend : IAlertDataToSend
-    {
-        public string Id { get; set; } = "d4fb8586-101f-4dff-a91e-2488b8214ba3";
-        public string Message { get; set; } = string.Empty;
-        public string Base64Image { get; set; } = string.Empty;
-        public int Category { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
+    public string Id { get; set; } = "d4fb8586-101f-4dff-a91e-2488b8214ba3";
+    public string Message { get; set; } = string.Empty;
+    public string Base64Image { get; set; } = string.Empty;
+    public int Category { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
