ab562761490a49531f7de44977204399b6e8b1ec
Konrad Blaszyński <77151114+XenkQ@users.noreply.github.com>
2024-09-02 10:01:24 +0200

    Making service from connection status and no connection displayer

---
+++ Services/ConnectionStatusService.cs
@@ -0,0 +1,41 @@
+﻿using MauiApp1.Scripts.Connection;
+
+namespace MauiApp1.Services;
+
+public enum ConnectionStatuses
+{
+    NoInternetConnection,
+    NoServerConnection,
+    NoServerAndInternetConnection,
+    Connected
+}
+
+public interface IConnectionStatusService
+{
+    ConnectionStatuses GetCurrentStatus();
+}
+
+public class ConnectionStatusService : IConnectionStatusService
+{
+    private readonly IServerConnectionChecker _serverConnectionChecker;
+
+    public ConnectionStatusService(IServerConnectionChecker serverConnectionChecker)
+    {
+        _serverConnectionChecker = serverConnectionChecker;
+    }
+
+    public ConnectionStatuses GetCurrentStatus()
+    {
+        bool isConnectedToNetwork = Connectivity.NetworkAccess == NetworkAccess.Internet;
+        bool isConnectedToServer = _serverConnectionChecker.IsConnected();
+
+        if (isConnectedToNetwork && !isConnectedToServer)
+            return ConnectionStatuses.NoServerConnection;
+        else if (!isConnectedToNetwork && isConnectedToServer)
+            return ConnectionStatuses.NoInternetConnection;
+        else if (!isConnectedToNetwork && !isConnectedToServer)
+            return ConnectionStatuses.NoServerAndInternetConnection;
+        else
+            return ConnectionStatuses.Connected;
+    }
+}
---
+++ Services/NoConnectionDisplayer.cs
@@ -0,0 +1,41 @@
+﻿using MauiApp1.View.StatusPages;
+
+namespace MauiApp1.Services;
+
+public interface INoConnectionDisplayer
+{
+    bool DisplayIfNoConnection();
+}
+
+public class NoConnectionDisplayer : INoConnectionDisplayer
+{
+    private readonly IConnectionStatusService _connectionStatusService;
+
+    public NoConnectionDisplayer(IConnectionStatusService connectionStatus)
+    {
+        _connectionStatusService = connectionStatus;
+    }
+
+    public bool DisplayIfNoConnection()
+    {
+        switch (_connectionStatusService.GetCurrentStatus())
+        {
+            case ConnectionStatuses.NoInternetConnection:
+                DisplayNoConnectionPage(ConnectionStatuses.NoInternetConnection);
+                return true;
+
+            case ConnectionStatuses.NoServerConnection:
+                DisplayNoConnectionPage(ConnectionStatuses.NoServerConnection);
+                return true;
+
+            case ConnectionStatuses.NoServerAndInternetConnection:
+                DisplayNoConnectionPage(ConnectionStatuses.NoServerAndInternetConnection);
+                return true;
+        }
+
+        return false;
+    }
+
+    private static void DisplayNoConnectionPage(ConnectionStatuses status)
+        => Shell.Current.GoToAsync($"{nameof(NoConnectionPage)}?connection_status={status}");
+}
