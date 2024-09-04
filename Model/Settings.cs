a2c9d583b38db214ffb86125f843f59c03167521
Konrad Blaszyński <77151114+XenkQ@users.noreply.github.com>
2024-09-04 15:17:20 +0200

    Changing app setting key names and adding json properties to settings model file

--- Model/Settings.cs
+++ Model/Settings.cs
@@ -1,4 +1,13 @@
-﻿namespace MauiApp1.Model.Settings;
+﻿using System.Text.Json.Serialization;
 
-public record ApiSettings(string BaseUrl, string StatusPath, string UploadPath);
-public record SettingsRoot(ApiSettings ApiSettings);
+namespace MauiApp1.Model.Settings;
+
+public record ApiSettings(
+    [property: JsonPropertyName("base_url")] string BaseUrl,
+    [property: JsonPropertyName("status_path")] string StatusPath,
+    [property: JsonPropertyName("upload_path")] string UploadPath
+);
+
+public record SettingsRoot(
+    [property: JsonPropertyName("api_setting")] ApiSettings ApiSettings
+);
